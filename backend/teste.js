f86d58d1b816046f852c431c72c5d0f1b7d2c1d4
cffricks <99102201+CFFricks@users.noreply.github.com>
2022-06-07 13:22:05 -0300

    arrumei a organizacao das pastas+cirei o crud da tabela Eixos

--- backend/teste.js
+++ backend/teste.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3061;
+const port = 3081;
 const sqlite3 = require('sqlite3').verbose();
 const DBPATH = 'banco.db';
-
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static("../FrontEnd"));
-
+app.use(express.static("../frontend"));
 app.use(express.json());
 
 
@@ -82,8 +80,77 @@ app.post('/agendadelete', urlencodedParser, (req, res) => {
 	db.close(); // Fecha o banco
 });
 
+//tabela eixo
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/Eixo', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM Eixo ORDER BY Nome COLLATE NOCASE';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/Eixoinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO Eixo (Nome, idAgenda) VALUES ('" + req.body.Nome + "','" + req.body.idAgenda + "')";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/EixoUpdate', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "UPDATE Eixo SET Nome = '" + req.body.Nome + "' WHERE idAgenda = " + req.body.idAgenda;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+
+// Exclui um registro do (é o D do CRUD - Delete)
+app.post('/Eixodelete', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM Eixo WHERE Nome = " + req.body.Nome;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+
+
 
 /* Inicia o servidor */
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
